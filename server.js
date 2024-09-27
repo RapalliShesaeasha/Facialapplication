9171caec87cf88ce90b38dd66f792e302436f946
Rapalli Shesaeasha <137075837+RapalliShesaeasha@users.noreply.github.com>
2024-09-27 10:24:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json({ limit: '10mb' })); // Increase limit here
 // Routes
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
